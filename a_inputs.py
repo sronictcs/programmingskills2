c193edbc2efa6396eeb110d3bef21a99a0d17c10
St Robert of Newminster Catholic School and Sixth Form ICT and CS Dept <47178190+sronictcs@users.noreply.github.com>
2020-09-25 11:56:11 +0100

    Add files via upload

---
+++ a_inputs.py
@@ -0,0 +1,56 @@
+#writing clear prompts
+#using user inputs
+
+#a input statement takes the user input and stores it in a variable
+name = input("Please enter your name: ")
+print (f"\nHello, {name}!")
+
+#3-1 create a variable called age and ask the user their age
+
+#to move on to a new line in Python we use \n
+print("Hello, i'm on one line \n and i'm on another.")
+
+#3-2 write a print statement over 2 lines using \n
+
+#3-3. Rental Car: Write a program that asks the user
+#what kind of rental car they would like.
+#Print a message about that car, such
+#as “Let me see if I can find you a Subaru.”
+
+
+#Python assumes all data being inputted is a string.
+#A string is a collection letters, numbers or characters.
+#strings are surrounded by quotes. Below is a string.
+tv_show = "the simpsons"
+
+#You can check what type of input a user has made by checking the type
+#of the variable holding the input using the type() function.
+age = input("What is your age")
+print(type(age))
+
+#another types of variable is an integer - or int (a whole number) you can
+#use a integer in a variable by typing it with no quote. See below:
+age = 15
+
+
+#To make a variable a specific data type like an int use the int function like
+#below:
+age = input("Enter your age")
+print(age)
+age = int(age)
+print (type(age))
+
+
+#Another way to do this is to apply the int function to the input function.
+#See below:
+age = int(input("Enter your age"))
+
+#3-4 use an input statement to store a users favourite number as an int
+
+
+
+#3-5 Write a program that takes a users input and returns the
+#number squared.
+    
+
+
